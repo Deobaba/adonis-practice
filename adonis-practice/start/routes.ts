b034067a66df3fcdf4020b4d12b9902c4c0f442b
Adenekan Adeola <104663055+Deobaba@users.noreply.github.com>
2024-03-05 11:07:09 +0100

    working on route

--- adonis-practice/start/routes.ts
+++ adonis-practice/start/routes.ts
@@ -27,3 +27,12 @@ Route.get('/', async (ctx:HttpContextContract) => {
   ctx.user = "testing"
   return { hello: 'world' }
 })
+// adding .where is for validation for the id to avoid conflict with other routes with similar path
+Route.get("/post/:id", async ({params})=> `get post with id ${params.id}`).where("id", /^[2-9]+$/)
+// route.matchers
+Route.get("/post/1",async()=> "this is for only 1")
+Route.get("/post",async()=> "create a post")
+Route.get('/post/topic/:topic?', ({params})=> `get post with topic ${params.topic}`).where("topic", /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/) // ? makes the parameter optional
+// wildcard route
+Route.get("/img/*", async ({params})=> params['*'][0])
+Route.get("/pic/:userid/*", async ({params})=> params['userid'] + " " + params['*'][0])
